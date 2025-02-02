ef34197f84cb67319f9fe4e343663f0bc4e503ea
GutmutigKafer <vladonozes@gmail.com>
2025-02-02 11:49:08 +0100

    JS test and Unstalations

---
+++ javascript/javascript1/week1/test/index.js
@@ -0,0 +1,25 @@
+let myName = "Vlad"
+console.log(myName)
+
+let myAge = 25
+console.log(myAge)
+
+const computer = "Macbook Pro"
+console.log(computer)
+
+let x = 5n ** 35n
+console.log(x)
+
+let myInfo = {
+    name: "Vlad",
+    age: 25,
+    computer: "Macbook Pro"
+}
+console.log(myInfo)
+console.log(myInfo.name)
+
+const myTeam = ['Amin', 'Anita', 'Aoon']
+console.log(myTeam[2])
+
+let modulo = 61 % 8
+console.log(modulo)
