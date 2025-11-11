37c38e9a92f085f424fa70c17c3dd013f645a951
GutmutigKafer <vladonozes@gmail.com>
2025-11-11 17:05:17 +0100

    Added fullName function

---
+++ courses/foundation/intro-to-javascript/week2/fullName.js
@@ -0,0 +1,16 @@
+//Flight booking fullname function
+
+const getFullName = (firstName, lastName, gender, useFormalName) => {
+  const formal = gender === "Male" ? "Monsieur " : "Madame ";
+  const message = `${useFormalName ? formal : ""}${firstName} ${lastName}`;
+  return firstName && lastName
+    ? message
+    : `Please enter both First name and Last name!`;
+};
+
+const fullname1 = getFullName("Vlad", "Krichman", "Male", false);
+const fullname2 = getFullName("Maria", "Kristensen", "Female", true);
+const fullname3 = getFullName("Francesc", "Sarda", "Male", true);
+const fullname4 = getFullName("", "Jonson", "Female", false);
+
+console.log(` ${fullname1} \n ${fullname2} \n ${fullname3} \n ${fullname4} `);
