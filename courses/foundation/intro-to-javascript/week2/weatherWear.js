8da9e6e2fac2bd9930b7d64cbbd1bea30e8bcd54
GutmutigKafer <vladonozes@gmail.com>
2025-11-11 17:27:47 +0100

    Added weatherWear function

---
+++ courses/foundation/intro-to-javascript/week2/weatherWear.js
@@ -0,0 +1,17 @@
+// Weather wear
+
+const whatToWear = (temp) => {
+  switch (true) {
+    case temp >= 40:
+      return "You don't need to wear anything!";
+    case temp >= 25:
+      return "You should wear a t-shirt";
+    case temp >= 10:
+      return "You should wear a sweater";
+    case temp >= -5:
+      return "You should wear a warm jacket";
+    default:
+      return "You should wear everything you can find!";
+  }
+};
+console.log(whatToWear(30));
