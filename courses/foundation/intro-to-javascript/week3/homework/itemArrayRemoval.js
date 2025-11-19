2b2cd948507c136346900f3cb9a998648fd8a4b6
GutmutigKafer <vladonozes@gmail.com>
2025-11-19 11:04:52 +0100

    Added itemArrayRemoval

---
+++ courses/foundation/intro-to-javascript/week3/homework/itemArrayRemoval.js
@@ -0,0 +1,32 @@
+const names = [
+  "Peter",
+  "Ahmad",
+  "Yana",
+  "kristina",
+  "Rasmus",
+  "Samuel",
+  "katrine",
+  "Tala",
+  "Ahmad",
+];
+const nameToRemove = "Ahmad";
+
+const removeName_v1 = () => {
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === nameToRemove) {
+      names.splice(i, 1);
+    }
+  }
+  return names;
+};
+console.log(removeName_v1());
+
+const removeName_v2 = (name, startFrom = 0) => {
+  if (!names.includes(name)) return `${name} is not on the list`;
+  const index = names.indexOf(name, startFrom);
+  names.splice(index, 1);
+  return `Removed ${name} (at index: ${index}) from the list \nUpdated list: \n${names.join(
+    ", "
+  )}`;
+};
+// console.log(removeName_v2(nameToRemove, 3));
